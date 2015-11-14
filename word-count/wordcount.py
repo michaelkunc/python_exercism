1085090e017eccb8a064198b9125dcdd4110c676
michaelkunc <mjkunc@gmail.com>
2015-11-14 13:36:53 -0600

    refactored word count

--- word-count/wordcount.py
+++ word-count/wordcount.py
@@ -1,9 +1,8 @@
+from collections import defaultdict
+
+
 def word_count(words):
-  word_count = {}
-  word_array = words.lower().split()
-  for word in word_array:
-    if word in word_count:
-      word_count[word] += 1
-    else:
-      word_count[word] = 1
-  return word_count
+    word_count = defaultdict(int)
+    for word in words.lower().split():
+        word_count[word] += 1
+    return word_count
