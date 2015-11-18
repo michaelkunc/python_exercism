455c8f4d85e3a3e6e088a74caee879164e25cf57
michaelkunc <mjkunc@gmail.com>
2015-11-17 18:55:27 -0600

    refactored rna solution

--- rna-transcription/dna.py
+++ rna-transcription/dna.py
@@ -1,6 +1,5 @@
+from string import maketrans
+
+
 def to_rna(strand):
-    translation = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-    rna_strand = []
-    for nucleotide in strand:
-        rna_strand.append(translation[nucleotide])
-    return ''.join(rna_strand)
+    return strand.translate(maketrans('GCTA', 'CGAU'))
