f447e4693dbd01d9a2da3f534add4dbd6f3b8d7f
michaelkunc <mjkunc@gmail.com>
2015-11-17 21:40:36 -0600

    revised hamming solution

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,2 @@
 def distance(first_strand, second_strand):
-    return len([a for a, b in zip(first_strand, second_strand) if a != b])
+    return sum(a != b  for a, b in zip(first_strand, second_strand))
