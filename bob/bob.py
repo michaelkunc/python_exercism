f882a30e1a91644597172133cccd5b92ef9316fd
michaelkunc <mjkunc@gmail.com>
2015-11-30 19:58:46 -0600

    refactored bob solution

--- bob/bob.py
+++ bob/bob.py
@@ -1,9 +1,10 @@
 def hey(what):
     what = what.strip()
+    response = 'Whatever.'
     if what.isupper():
-        return 'Whoa, chill out!'
-    if not what:
-        return 'Fine. Be that way!'
-    if what.endswith('?'):
-        return 'Sure.'
-    return 'Whatever.'
+        response = 'Whoa, chill out!'
+    elif not what:
+        response = 'Fine. Be that way!'
+    elif what.endswith('?'):
+        response = 'Sure.'
+    return response
