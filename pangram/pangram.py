572d19affd23146de238862026f2d5ee544568f9
michaelkunc <mjkunc@gmail.com>
2015-12-07 16:17:41 -0600

    revised pangram solution

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,7 +1,4 @@
-import string
-
 
 def is_pangram(sentence):
     sentence = sentence.decode('unicode_escape').encode('ascii', 'ignore')
-    unique_letters = list(set(''.join(c.lower() for c in sentence  if str.isalpha(c))))
-    return len(unique_letters) == 26
+    return len(set([c for c in sentence.lower() if str.isalpha(c)])) == 26
