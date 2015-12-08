de15f475f117e032d68a8b92a8044bb8161832ed
michaelkunc <mjkunc@gmail.com>
2015-12-08 14:48:15 -0600

    start of meetup solution

---
+++ meetup/meetup.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+def meetup_day(year, month, day_of_week, day_of_month):
+    days = {'Monday' : 0, 'Tuesday' : 1, 'Wednesday' : 2,
+      'Thursday':3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
+    if day_of_month == '1st':
+
+    return date(year, month,)
+
+
+
