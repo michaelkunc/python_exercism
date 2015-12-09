980ccddca724937f08abb78496acc0e7d9b49b1f
michaelkunc <mjkunc@gmail.com>
2015-12-08 21:18:33 -0600

    initial meetup solution

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -1,11 +1,18 @@
 from datetime import date
 
-def meetup_day(year, month, day_of_week, day_of_month):
-    days = {'Monday' : 0, 'Tuesday' : 1, 'Wednesday' : 2,
-      'Thursday':3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
-    if day_of_month == '1st':
 
-    return date(year, month,)
+def meetup_day(year, month, day, week):
+    days = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2,
+            'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
+    date_ranges = {'teenth': [13, 19], '1st':[1, 8], '2nd': [8,15], '3rd': [15, 22],
+        '4th':[22, 29], 'last':[25, 32], 'last_week_in_feb': [22,30]}
 
+    if week == '5th':
+        raise Exception("Please enter 'last' instead of '5th'")
 
+    if month == 2 and week == 'last' and day == 'Sunday':
+        week = 'last_week_in_feb'
 
+    for i in range(date_ranges[week][0], date_ranges[week][1]):
+        if (date(year, month, i).weekday()) == days[day]:
+            return date(year, month, i)
