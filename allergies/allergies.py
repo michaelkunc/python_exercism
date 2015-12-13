4f12f4c412b107494c98fed61303805232cd5257
michaelkunc <mjkunc@gmail.com>
2015-12-13 15:10:49 -0600

    allergy solution in process

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,10 +1,14 @@
 
 class Allergies:
-  allergens = {'eggs' : 1, 'peanuts' : 2, 'shellfish' : 4, 'strawberries' : 8, 'tomatoes' : 16, 'chocolate' : 32, 'pollen' : 64, 'cats' : 128}
+    allergens = {'eggs': 1, 'peanuts': 2, 'shellfish': 4, 'strawberries': 8,
+                 'tomatoes': 16, 'chocolate': 32, 'pollen': 64, 'cats': 128}
 
-  def __init__(self, score=0):
-    self.allergen_list = []
-    self.score = 0
+    def __init__(self, score):
+        self.score = score
+        self.list = self.allergen_list()
 
-  def is_allergic_to(self, allergen):
-    return "is this thing on?"
+    def allergen_list(self):
+        return sorted([x for x in self.allergens if self.is_allergic_to(x)], key=lambda i: self.allergens[i])
+
+    def is_allergic_to(self, allergen):
+        return self.allergens[allergen] & self.score
--- allergies/allergies_test.py
+++ allergies/allergies_test.py
@@ -11,14 +11,14 @@ class AllergiesTests(unittest.TestCase):
         self.assertFalse(allergies.is_allergic_to('cats'))
         self.assertFalse(allergies.is_allergic_to('strawberries'))
 
-    # def test_is_allergic_to_eggs(self):
-    #     self.assertTrue(Allergies(1).is_allergic_to('eggs'))
+    def test_is_allergic_to_eggs(self):
+        self.assertTrue(Allergies(1).is_allergic_to('eggs'))
 
-    # def test_has_the_right_allergies(self):
-    #     allergies = Allergies(5)
-    #     self.assertTrue(allergies.is_allergic_to('eggs'))
-    #     self.assertTrue(allergies.is_allergic_to('shellfish'))
-    #     self.assertFalse(allergies.is_allergic_to('strawberries'))
+    def test_has_the_right_allergies(self):
+        allergies = Allergies(5)
+        self.assertTrue(allergies.is_allergic_to('eggs'))
+        self.assertTrue(allergies.is_allergic_to('shellfish'))
+        self.assertFalse(allergies.is_allergic_to('strawberries'))
 
     # def test_no_allergies_at_all(self):
     #     self.assertEqual([], Allergies(0).lst)
