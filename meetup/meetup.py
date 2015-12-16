585c4fa88305a3a2adcf7cdae92078fc432d13ff
michaelkunc <mjkunc@gmail.com>
2015-12-15 21:16:12 -0600

    revised meetup solution in progress

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -3,16 +3,30 @@ import calendar
 
 
 def meetup_day(year, month, day, week):
-    days = dict(zip(list(calendar.day_name), range(7)))
-    date_ranges = {'teenth': [13, 19], '1st':[1, 8], '2nd': [8,15], '3rd': [15, 22],
-        '4th':[22, 29], 'last':[25, 32], 'last_week_in_feb': [22,30]}
+    days = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2,
+            'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
 
-    if week == '5th':
-        raise Exception("Please enter 'last' instead of '5th'")
+    day_of_month = [d for d in calendar.Calendar().itermonthdates(year, month) if d.weekday() == days[day] and d.month == month]
 
-    if month == 2 and week == 'last' and day == 'Sunday':
-        week = 'last_week_in_feb'
+    if week == '1st':
+        return day_of_month[0]
 
-    for i in range(date_ranges[week][0], date_ranges[week][1]):
-        if (date(year, month, i).weekday()) == days[day]:
-            return date(year, month, i)
+    elif week == 'last':
+        return day_of_month[-1]
+
+    elif week == '2nd':
+        return day_of_month[1]
+
+    elif week == '3rd':
+        return day_of_month[2]
+
+    elif week == 'teenth':
+        return day_of_month[2]
+
+
+
+    # for i in range(date_ranges[week][0], date_ranges[week][1]):
+    #     if (date(year, month, i).weekday()) == days[day]:
+    #         return date(year, month, i)
+
+
