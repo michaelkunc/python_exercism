9e6765a11f9f80a1f2a52fa61df99b92f9bc629c
michaelkunc <mjkunc@gmail.com>
2015-12-17 06:47:56 -0600

    refactored anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,8 +1,16 @@
 
 def detect_anagrams(word, possible_anagrams):
-    return [x for x in possible_anagrams if is_anagram(word, x)]
+    return [x for x in possible_anagrams if ''.join(sorted(x.lower())) == ''.join(sorted(word.lower()))
 
-def is_anagram(first_word, second_word):
-    first_word = first_word.lower()
-    second_word = second_word.lower()
-    return first_word != second_word and sorted(first_word) == sorted(second_word)
+
+
+
+
+
+# def detect_anagrams(word, possible_anagrams):
+#     return [x for x in possible_anagrams if is_anagram(word, x)]
+
+# def is_anagram(first_word, second_word):
+#     first_word = first_word.lower()
+#     second_word = second_word.lower()
+#     return first_word != second_word and sorted(first_word) == sorted(second_word)
