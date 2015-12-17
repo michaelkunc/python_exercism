b9be1dc095d7f56065fc6d06c0be6329d70f0928
michaelkunc <mjkunc@gmail.com>
2015-12-17 06:41:39 -0600

    revised anagram solution

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,3 +1,8 @@
 
 def detect_anagrams(word, possible_anagrams):
-    return [x for x in possible_anagrams if ''.join(sorted(x.lower())) == ''.join(sorted(word.lower())) and x.lower() != word.lower()]
+    return [x for x in possible_anagrams if is_anagram(word, x)]
+
+def is_anagram(first_word, second_word):
+    first_word = first_word.lower()
+    second_word = second_word.lower()
+    return first_word != second_word and sorted(first_word) == sorted(second_word)
