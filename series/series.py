c552e1af3ab18bbf3935f952245f17719fa203ea
michaelkunc <mjkunc@gmail.com>
2015-12-18 17:23:37 -0600

    first test passing in series

---
+++ series/series.py
@@ -0,0 +1,6 @@
+def slices(number, series_length):
+
+    number_list = list(number)
+    number_list = [map(int, i) for i in number_list]
+    result_list = [number_list[i] for i in range(0, len(number_list), series_length )]
+    return result_list
