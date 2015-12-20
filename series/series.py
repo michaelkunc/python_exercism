a6c2bc6605ffde9120a1fe23313260b0b1740c74
michaelkunc <mjkunc@gmail.com>
2015-12-20 09:00:06 -0500

    solution in progress

--- series/series.py
+++ series/series.py
@@ -1,8 +1,18 @@
-from itertools import permutations
+import itertools
 
 def slices(number, series_length):
 
+    # number_list = list(number)
+    # number_list = [map(int, i) for i in number_list]
+    # result_list = [number_list[i] for i in range(0, len(number_list), series_length )]
+    # return result_list
+
+    results = []
     number_list = list(number)
-    number_list = [map(int, i) for i in number_list]
-    result_list = [number_list[i] for i in range(0, len(number_list), series_length )]
-    return result_list
+    number_list = [int(i) for i in number_list]
+    combinations = itertools.combinations(number_list, series_length)
+    combinations = [list(i) for i in combinations]
+    for i in combinations:
+        if i not in results:
+            results.append(i)
+    return results
