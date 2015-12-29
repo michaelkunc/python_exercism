8a57aaf30da13545876093f0f5c48613e44e4cdf
michaelkunc <mjkunc@gmail.com>
2015-12-29 11:02:17 -0600

    revised dnasolution

--- rna-transcription/dna.py
+++ rna-transcription/dna.py
@@ -1,5 +1,7 @@
 from string import maketrans
 
+DNA = 'GCTA'
+RNA = 'CGAU'
 
 def to_rna(strand):
-    return strand.translate(maketrans('GCTA', 'CGAU'))
+    return strand.translate(maketrans(DNA, RNA))
