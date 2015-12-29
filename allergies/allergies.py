303e56710e2d7c722a2391539c44de2456f6eb06
michaelkunc <mjkunc@gmail.com>
2015-12-29 10:13:51 -0600

    revised allergies solution

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,14 +1,22 @@
+class Allergies(object):
 
-class Allergies:
-    allergens = {'eggs': 1, 'peanuts': 2, 'shellfish': 4, 'strawberries': 8,
-                 'tomatoes': 16, 'chocolate': 32, 'pollen': 64, 'cats': 128}
+    ALLERGENS = {
+        "eggs":         1,
+        "peanuts":      2,
+        "shellfish":    4,
+        "strawberries": 8,
+        "tomatoes":     16,
+        "chocolate":    32,
+        "pollen":       64,
+        "cats":         128
+    }
 
     def __init__(self, score):
         self.score = score
-        self.list = self.allergen_list()
 
-    def allergen_list(self):
-        return sorted([x for x in self.allergens if self.is_allergic(x)], key=lambda i: self.allergens[i])
+    def is_allergic_to(self, allergen):
+        return bool(self.ALLERGENS[allergen] & self.score)
 
-    def is_allergic(self, allergen):
-        return self.allergens[allergen] & self.score
+    @property
+    def lst(self):
+        return list(filter(self.is_allergic_to, self.ALLERGENS))
--- allergies/allergies_test.py
+++ allergies/allergies_test.py
@@ -21,16 +21,16 @@ class AllergiesTests(unittest.TestCase):
         self.assertFalse(allergies.is_allergic_to('strawberries'))
 
     def test_no_allergies_at_all(self):
-        self.assertEqual([], Allergies(0).list)
+        self.assertEqual([], Allergies(0).lst)
 
     def test_allergic_to_just_peanuts(self):
-        self.assertEqual(['peanuts'], Allergies(2).list)
+        self.assertEqual(['peanuts'], Allergies(2).lst)
 
     def test_allergic_to_everything(self):
         self.assertEqual(
             sorted(('eggs peanuts shellfish strawberries tomatoes '
                     'chocolate pollen cats').split()),
-            sorted(Allergies(255).list))
+            sorted(Allergies(255).lst))
 
     # @unittest.skip('Extra Credit: Passes with a specific type of solution')
     # def test_ignore_non_allergen_score_parts(self):
