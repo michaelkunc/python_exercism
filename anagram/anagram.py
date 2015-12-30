98983441e80b93dc958efb52c2c14d7e9d895710
michaelkunc <mjkunc@gmail.com>
2015-12-30 11:37:04 -0600

    new version of anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,8 +1,5 @@
 
 def detect_anagrams(word, possible_anagrams):
-    return [x for x in possible_anagrams if is_anagram(word, x)]
+    word = word.lower()
+    return filter(lambda x: sorted(x.lower()) == sorted(word) and x.lower() != word, possible_anagrams)
 
-def is_anagram(first_word, second_word):
-    first_word = first_word.lower()
-    second_word = second_word.lower()
-    return first_word != second_word and sorted(first_word) == sorted(second_word)
