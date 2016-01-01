f1d16e4cec6fb9289ba177ccb3b5b03a7614a373
michaelkunc <mjkunc@gmail.com>
2016-01-01 12:11:05 -0600

    atbash cipher solution

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,17 +1,23 @@
 import string
 
-DIGITS = ''.join(map(str, range(0,10)))
+DIGITS = ''.join(map(str, range(0, 10)))
 GROUP_SIZE = 5
 PLAIN = string.ascii_lowercase
 CIPHER = PLAIN[::-1]
 
 
-
 def encode(plaintext):
-    clean_plaintext = ''.join(l.lower() for l in plaintext if l.isalnum())
-    ciphertext = clean_plaintext.translate(string.maketrans(PLAIN+DIGITS, CIPHER+DIGITS))
-    return ' '.join([ciphertext[i:i+GROUP_SIZE] for i in range(0, len(ciphertext), GROUP_SIZE)])
+    ciphertext = encode_decode(clean_text(plaintext), PLAIN, CIPHER)
+    return ' '.join([ciphertext[i:i + GROUP_SIZE] for i in range(0, len(ciphertext), GROUP_SIZE)])
 
 
 def decode(ciphertext):
-    return encode(ciphertext)
+    return encode_decode(clean_text(ciphertext), CIPHER, PLAIN)
+
+
+def clean_text(text):
+    return ''.join(l.lower() for l in text if l.isalnum())
+
+
+def encode_decode(text, plain, cipher):
+    return text.translate(string.maketrans(plain, cipher))
