7a15f45cf341d620b85d4df4157ab002a6d26c82
michaelkunc <mjkunc@gmail.com>
2016-01-15 16:16:38 -0600

    sieve refactor in process

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -3,6 +3,7 @@ import math
 def sieve(upper_limit):
     return [x for x in range(2,upper_limit + 1) if is_prime(x)]
 
+
 def is_prime(number):
     if number % 2 == 0 and number > 2:
         return False
