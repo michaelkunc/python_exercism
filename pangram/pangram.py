10991e68de0adf73f71e01361d450e2c54327e20
michaelkunc <mjkunc@gmail.com>
2016-05-04 21:44:22 -0500

    revised pangram solution

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -2,5 +2,4 @@ import string
 
 def is_pangram(sentence):
     sentence = set(sentence.lower().replace(' ',''))
-    alphabet = set(string.ascii_lowercase)
-    return len(alphabet - sentence) == 0
+    return len(set(string.ascii_lowercase) - sentence) == 0
