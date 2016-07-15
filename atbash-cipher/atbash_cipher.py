cc71e4cabdb05eb7f87a1e068883c61a90f45731
Michael Kunc <mjkunc@gmail.com>
2016-07-15 12:43:12 -0700

    refactor to atbash_cipher solution

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,23 +1,22 @@
 import string
 
-DIGITS = ''.join(map(str, range(0, 10)))
 GROUP_SIZE = 5
 PLAIN = string.ascii_lowercase
 CIPHER = PLAIN[::-1]
-
+ENCODING = string.maketrans(PLAIN, CIPHER)
 
 def encode(plaintext):
-    ciphertext = encode_decode(clean_text(plaintext), PLAIN, CIPHER)
+    ciphertext = encode_decode(clean_text(plaintext))
     return ' '.join([ciphertext[i:i + GROUP_SIZE] for i in range(0, len(ciphertext), GROUP_SIZE)])
 
 
 def decode(ciphertext):
-    return encode_decode(clean_text(ciphertext), CIPHER, PLAIN)
+    return encode_decode(clean_text(ciphertext))
 
 
 def clean_text(text):
     return ''.join(l.lower() for l in text if l.isalnum())
 
 
-def encode_decode(text, plain, cipher):
-    return text.translate(string.maketrans(plain, cipher))
+def encode_decode(text):
+    return text.translate(ENCODING)
