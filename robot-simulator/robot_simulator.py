4de57ed1cc071ebe9260810b2401f984fb49bd38
michaelkunc <mjkunc@gmail.com>
2016-07-18 11:13:42 -0700

    slight refactor to turn methods

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -21,15 +21,16 @@ class Robot(object):
             self.coordinates = (coordinate1, coordinate2)
 
     def turn_right(self):
-        current_bearing = DIRECTIONS.index(self.bearing)
-        return self.turn(current_bearing, operator.lt(current_bearing, 3), operator.add(current_bearing, 1), 0)
+        return self.turn(self.bearing_index(), operator.lt(self.bearing_index(), 3), operator.add(self.bearing_index(), 1), 0)
 
     def turn_left(self):
-        current_bearing = DIRECTIONS.index(self.bearing)
-        return self.turn(current_bearing, operator.gt(current_bearing, 0), operator.sub(current_bearing, 1), -1)
+        return self.turn(self.bearing_index(), operator.gt(self.bearing_index(), 0), operator.sub(self.bearing_index(), 1), -1)
 
     def turn(self, current_bearing, lt_gt, add_sub, else_index):
         if lt_gt:
             self.bearing = DIRECTIONS[add_sub]
         else:
             self.bearing = DIRECTIONS[else_index]
+
+    def bearing_index(self):
+        return DIRECTIONS.index(self.bearing)
