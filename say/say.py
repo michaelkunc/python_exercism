1eca843e9f6ef36ce3c540feecafd444c71ac105
michaelkunc <mjkunc@gmail.com>
2016-10-14 15:20:18 -0700

    start of say refactor

--- say/say.py
+++ say/say.py
@@ -1,120 +1,128 @@
 
-DIGITS_TO_WORDS = {
-    '0': "zero",
-    '1': "one",
-    '2': "two",
-    '3': "three",
-    '4': "four",
-    '5': "five",
-    '6': "six",
-    '7': "seven",
-    '8': "eight",
-    '9': "nine",
-    '10': "ten",
-    '11': "eleven",
-    '12': "twelve",
-    '13': "thirteen",
-    '14': "fourteen",
-    '15': "fifteen",
-    '16': "sixteen",
-    '17': "seventeen",
-    '18': "eighteen",
-    '19': "nineteen",
-    '20': "twenty",
-    '30': "thirty",
-    '40': "forty",
-    '50': "fifty",
-    '60': "sixty",
-    '70': "seventy",
-    '80': "eighty",
-    '90': "ninety"
+TENS_TEENS_ONES = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety"
 }
 
-MAGNITUDES = ['hundred', 'thousand', 'million', 'billion']
-
+# MAGNITUDES = ['hundred', 'thousand', 'million', 'billion']
 
 def say(integer):
-    integer = str(int(integer))
-    out_of_range(integer)
-    split_integer = divy_into_hundreds(integer)
-    if len(integer) >= 10 and len(integer) <= 13 :
-        if check_for_all_zeros(split_integer):
-            return simple_number(split_integer, MAGNITUDES[3])
-        else:
-            return ''.join([handle_the_hundreds(split_integer[0]), ' ', MAGNITUDES[3], ' ', handle_the_hundreds(split_integer[1]), ' ', MAGNITUDES[2], ' ', handle_the_hundreds(split_integer[2]), ' ', MAGNITUDES[1], ' ', handle_the_hundreds(split_integer[3])])
-    elif len(integer) == 7:
-        if check_for_all_zeros(split_integer):
-            return simple_number(split_integer, MAGNITUDES[2])
-        elif split_integer[1] == '000':
-            return ''.join([less_than_21(split_integer[0]), ' ', MAGNITUDES[2], ' and ', less_than_21(split_integer[2])])
-        else:
-            return ''.join([less_than_21(split_integer[0]), ' ', MAGNITUDES[2], ' ', less_than_21(split_integer[1]), ' ', MAGNITUDES[1], ' ', handle_the_hundreds(split_integer[2])])
-    elif len(integer) == 4:
-        if check_for_all_zeros(split_integer):
-            return simple_number(split_integer, MAGNITUDES[1])
-        else:
-            return thousands(split_integer,MAGNITUDES[1])
-    elif len(integer) == 3:
-        return handle_the_hundreds(integer)
-    elif int(integer) > 20:
-        return less_than_100(integer)
+    if integer in TENS_TEENS_ONES.keys():
+        return TENS_TEENS_ONES[integer]
     else:
-        return less_than_21(integer)
+        tens, ones = divmod(integer, 10)[0] * 10, divmod(integer, 10)[1]
+        return ''.join([say(tens), '-', say(ones)])
 
 
-def less_than_21(integer):
-    integer = str(int(integer))
-    return DIGITS_TO_WORDS[integer]
 
 
-def less_than_100(integer):
-    return DIGITS_TO_WORDS[integer[0] + '0'] + '-' + DIGITS_TO_WORDS[integer[1]]
+
+# def say(integer):
+#     integer = str(int(integer))
+#     out_of_range(integer)
+#     split_integer = divy_into_hundreds(integer)
+#     if len(integer) >= 10 and len(integer) <= 13:
+#         if check_for_all_zeros(split_integer):
+#             return simple_number(split_integer, MAGNITUDES[3])
+#         else:
+#             return ''.join([handle_the_hundreds(split_integer[0]), ' ', MAGNITUDES[3], ' ', handle_the_hundreds(split_integer[1]), ' ', MAGNITUDES[2], ' ', handle_the_hundreds(split_integer[2]), ' ', MAGNITUDES[1], ' ', handle_the_hundreds(split_integer[3])])
+#     elif len(integer) == 7:
+#         if check_for_all_zeros(split_integer):
+#             return simple_number(split_integer, MAGNITUDES[2])
+#         elif split_integer[1] == '000':
+#             return ''.join([less_than_21(split_integer[0]), ' ', MAGNITUDES[2], ' and ', less_than_21(split_integer[2])])
+#         else:
+#             return ''.join([less_than_21(split_integer[0]), ' ', MAGNITUDES[2], ' ', less_than_21(split_integer[1]), ' ', MAGNITUDES[1], ' ', handle_the_hundreds(split_integer[2])])
+#     elif len(integer) == 4:
+#         if check_for_all_zeros(split_integer):
+#             return simple_number(split_integer, MAGNITUDES[1])
+#         else:
+#             return thousands(split_integer, MAGNITUDES[1])
+#     elif len(integer) == 3:
+#         return handle_the_hundreds(integer)
+#     elif int(integer) > 20:
+#         return less_than_100(integer)
+#     else:
+#         return less_than_21(integer)
 
 
-def less_than_1000(integer):
-    return DIGITS_TO_WORDS[integer[0]] + ' ' + MAGNITUDES[0]
+# def less_than_21(integer):
+#     return DIGITS_TO_WORDS[str(int(integer))]
 
 
-def handle_the_hundreds(integer):
-    result = []
-    result.append(less_than_1000(integer))
-    integer = int(integer[1:])
-    if integer > 0 and integer < 21:
-        result.append(' and ' + less_than_21(str(integer)))
-    elif integer > 0:
-        result.append(' and ' + less_than_100(str(integer)))
-    return ''.join(result)
+# def less_than_100(integer):
+#     return DIGITS_TO_WORDS[integer[0] + '0'] + '-' + DIGITS_TO_WORDS[integer[1]]
 
 
-def divy_into_hundreds(integer):
-    leading_digits = len(integer) % 3
-    if leading_digits == 0:
-        return split_remaining_digits(integer)
-    else:
-        return split_leading_digits(integer, leading_digits) + split_remaining_digits(integer[leading_digits:])
+# def less_than_1000(integer):
+#     return DIGITS_TO_WORDS[integer[0]] + ' ' + MAGNITUDES[0]
 
 
-def split_leading_digits(integer, leading_digits):
-    return [integer[0:leading_digits]]
+# def handle_the_hundreds(integer):
+#     result = []
+#     result.append(less_than_1000(integer))
+#     integer = int(integer[1:])
+#     if integer > 0 and integer < 21:
+#         result.append(' and ' + less_than_21(str(integer)))
+#     elif integer > 0:
+#         result.append(' and ' + less_than_100(str(integer)))
+#     return ''.join(result)
 
 
-def split_remaining_digits(integer):
-    return [integer[i:i + 3] for i in range(0, len(integer), 3)]
+# def divy_into_hundreds(integer):
+#     leading_digits = len(integer) % 3
+#     if leading_digits == 0:
+#         return split_remaining_digits(integer)
+#     else:
+#         return split_leading_digits(integer, leading_digits) + split_remaining_digits(integer[leading_digits:])
 
 
-def check_for_all_zeros(split_integer):
-    return set(split_integer[1:]) == {'000'}
+# def split_leading_digits(integer, leading_digits):
+#     return [integer[0:leading_digits]]
 
 
-def simple_number(split_integer, magnitude):
-    return ''.join([less_than_21(split_integer[0]), ' ', magnitude])
+# def split_remaining_digits(integer):
+#     return [integer[i:i + 3] for i in range(0, len(integer), 3)]
 
-def thousands(split_integer, magnitude):
-    return ''.join([less_than_21(split_integer[0]), ' ', magnitude, ' ', handle_the_hundreds(split_integer[1])])
 
-def out_of_range(integer):
-    # integer = str(integer)
-    if integer[0]  == '-' :
-        raise AttributeError('integer must be greater than 0')
-    elif len(integer) > 12:
-        raise AttributeError('integer too big!')
+# def check_for_all_zeros(split_integer):
+#     return set(split_integer[1:]) == {'000'}
+
+
+# def simple_number(split_integer, magnitude):
+#     return ''.join([less_than_21(split_integer[0]), ' ', magnitude])
+
+
+# def thousands(split_integer, magnitude):
+#     return ''.join([less_than_21(split_integer[0]), ' ', magnitude, ' ', handle_the_hundreds(split_integer[1])])
+
+
+# def out_of_range(integer):
+#     if integer[0] == '-' or len(integer) > 12:
+#         raise AttributeError('integer not in accepted range')
