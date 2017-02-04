9b71b495bcc50b5d09bddb8669fef212b7ad304f
michaelkunc <mjkunc@gmail.com>
2017-02-04 10:42:52 -0800

    refactor to clock

--- clock/clock.py
+++ clock/clock.py
@@ -3,29 +3,20 @@ class Clock(object):
     MINUTES = 60
 
     def __init__(self, hours, minutes):
-        self.hours = hours
-        self.minutes = minutes
+        self.minutes = (hours * Clock.MINUTES) + minutes
 
-    def __str__(self):
-        self.roll_over()
-        return self.format(Clock.HOURS, self.hours) + ':' + self.format(Clock.MINUTES, self.minutes)
+    def __repr__(self):
+        hours, minutes = self.hours_minutes(self.minutes)
+        return '{:02}:{:02}'.format(hours, minutes)
 
-    def format(self, unit_of_time, number_of_units):
-        if (number_of_units % unit_of_time) < 10:
-            return '0' + str(number_of_units % unit_of_time)
-        else:
-            return str(number_of_units % unit_of_time)
-
-    def roll_over(self):
-        self.hours = self.hours + self.minutes / Clock.MINUTES
-        self.minutes = self.minutes % Clock.MINUTES
-        return self
+    def hours_minutes(self, minutes):
+        hours, minutes = divmod(minutes, Clock.MINUTES)
+        hours = hours % Clock.HOURS
+        return hours, minutes
 
     def add(self, additional_minutes):
         self.minutes = self.minutes + additional_minutes
         return self
 
     def __eq__(self, other):
-        if type(other) is type(self):
-            return str(self) == str(other)
-        return False
+        return str(self) == str(other)
