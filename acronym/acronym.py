ebaeee12c69d054ca8be14c98dd4cb2b2f6da8a1
michaelkunc <mjkunc@gmail.com>
2017-02-07 17:55:29 -0800

    midwy through a refactor

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,9 +1,5 @@
+import re
 
 
 def abbreviate(phrase):
-    if ':' in phrase:
-        phrase = phrase[:phrase.index(':')]
-    phrase = phrase.replace('-', ' ')
-    letters = [x for ind, x in enumerate(
-        phrase) if x == x.upper() or phrase[ind - 1] == ' ']
-    return ''.join([i.upper() for i in letters]).replace(' ', '').replace(',', '')
+    return ''.join([item[0].upper() for item in re.findall(r'\S+', phrase)])
