8c19222131b656da4118884311f5ca1295d8e10c
michaelkunc <mjkunc@gmail.com>
2017-02-21 20:52:44 -0800

    first test of kindergarden

---
+++ kindergarten-garden/kindergarten_garden.py
@@ -0,0 +1,17 @@
+class Garden(object):
+    PLANTS = {'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'}
+
+    STUDENTS = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred',
+                'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
+
+    def __init__(self, rows):
+        self.rows = rows.split('\n')
+        self.plant_names = self.plant_names(list(rows.replace('\n', '')))
+
+    def plants(self, student):
+        columns = Garden.STUDENTS.index(
+            student), Garden.STUDENTS.index(student) + 1
+        return self.plant_names
+
+    def plant_names(self, rows):
+        return [Garden.PLANTS[i] for i in rows]
