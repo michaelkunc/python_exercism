77cecb0077448fd0677f819c634ed05647926d7d
michaelkunc <mjkunc@gmail.com>
2017-02-24 08:34:22 -0800

    3rd test passing

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -4,18 +4,18 @@ class Garden(object):
     STUDENTS = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred',
                 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
 
-    CUPS = [range(0, 12)[i:i + 2] for i in range(0, 12, 2)]
+    CUPS = [range(0, 24)[i:i + 2] for i in range(0, 24, 2)]
 
     def __init__(self, rows):
         self.rows = [list(i) for i in rows.split('\n')]
 
     def plants(self, student):
         plant_names = []
-        loc = self.student_locations()
-        loc = loc[student]
+        loc = self.student_locations(student)
         for r in self.rows:
             plant_names.extend([r[loc[0]], r[loc[1]]])
         return [Garden.PLANTS[p] for p in plant_names]
 
-    def student_locations(self):
-        return dict(zip(Garden.STUDENTS, Garden.CUPS))
+    def student_locations(self, student):
+        cup_locations = dict(zip(Garden.STUDENTS, Garden.CUPS))
+        return cup_locations[student]
--- kindergarten-garden/kindergarten_garden_test.py
+++ kindergarten-garden/kindergarten_garden_test.py
@@ -11,19 +11,19 @@ class KindergartenGardenTests(unittest.TestCase):
 
     def test_bob_and_charlies_gardens(self):
         garden = Garden("VVCCGG\nVVCCGG")
-    #     self.assertEqual(["Clover"] * 4, garden.plants("Bob"))
-        # self.assertEqual(["Grass"] * 4, garden.plants("Charlie"))
+        self.assertEqual(["Clover"] * 4, garden.plants("Bob"))
+        self.assertEqual(["Grass"] * 4, garden.plants("Charlie"))
 
-    # def test_full_garden(self):
-    #     garden = Garden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
-    #     self.assertEqual("Violets Radishes Violets Radishes".split(),
-    #                      garden.plants("Alice"))
-    #     self.assertEqual("Clover Grass Clover Clover".split(),
-    #                      garden.plants("Bob"))
-    #     self.assertEqual("Grass Clover Clover Grass".split(),
-    #                      garden.plants("Kincaid"))
-    #     self.assertEqual("Grass Violets Clover Violets".split(),
-    #                      garden.plants("Larry"))
+    def test_full_garden(self):
+        garden = Garden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
+        self.assertEqual("Violets Radishes Violets Radishes".split(),
+                         garden.plants("Alice"))
+        self.assertEqual("Clover Grass Clover Clover".split(),
+                         garden.plants("Bob"))
+        self.assertEqual("Grass Clover Clover Grass".split(),
+                         garden.plants("Kincaid"))
+        self.assertEqual("Grass Violets Clover Violets".split(),
+                         garden.plants("Larry"))
 
     # def test_disordered_test(self):
     #     garden = Garden("VCRRGVRG\nRVGCCGCV",
