13d6e39d8d917355a7d5950395f1fad6a4b5f012
michaelkunc <mjkunc@gmail.com>
2017-02-28 16:38:12 -0800

    tests passing flatten_array

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,15 +1,14 @@
 def flatten(nested_list):
     flattend_array = []
-    x = check_type_and_add(nested_list, flattend_array)
-    return x[1]
+    return check_type_and_add(nested_list, flattend_array)
 
 
 def check_type_and_add(nested_list, array):
     for i in nested_list:
-        if type(i) is not list:
-            array.append(i)
-        elif len(i) == 0:
+        if type(i) is tuple or i is None:
             pass
+        elif type(i) is not list:
+            array.append(i)
         else:
             check_type_and_add(i, array)
-    return (nested_list, array)
+    return array
