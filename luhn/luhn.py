82be69a1765c2c7607539fb7d77737f165b959bc
michaelkunc <mjkunc@gmail.com>
2017-03-28 15:38:27 -0700

    slight refactor to luhn

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,20 +1,23 @@
 
 class Luhn(object):
 
-    def __init__(self, id_string):
-        self.id_string = id_string.replace(' ', '')
+    def __init__(self, digit_string):
+        self.digit_string = digit_string.replace(' ', '')
 
     def is_valid(self):
-        if len(self.id_string) < 2 or not self.id_string.isdigit():
+        if len(self.digit_string) < 2 or not self.digit_string.isdigit():
             return False
         else:
-            total = 0
-            for ix, num in enumerate(reversed(self.id_string)):
-                num = int(num)
-                if ix % 2 != 0 and num * 2 > 9:
-                    total += (num * 2) - 9
-                elif ix % 2 != 0:
-                    total += (num * 2)
-                else:
-                    total += num
-        return total % 10 == 0
+            return self.calculate_total() % 10 == 0
+
+    def calculate_total(self):
+        total = 0
+        for ix, num in enumerate(reversed(self.digit_string)):
+            num = int(num)
+            if ix % 2 != 0 and num * 2 > 9:
+                total += (num * 2) - 9
+            elif ix % 2 != 0:
+                total += (num * 2)
+            else:
+                total += num
+        return total
