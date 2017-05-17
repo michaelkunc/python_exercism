b5c4b5b863666ba8845dce000082889abfc6fa07
michaelkunc <mjkunc@gmail.com>
2017-05-17 16:11:25 -0700

    first solution

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,11 +1,17 @@
 import string
 
-key = string.ascii_letters
+key = string.ascii_lowercase
 
 
 def rotate(plain_text, shift_key):
     cipher_text = []
     for item in plain_text:
-        i = (key.index(item) + shift_key) % 26
-        cipher_text.append(key[i])
+        try:
+            i = (key.index(item.lower()) + shift_key) % 26
+            if item.isupper():
+                cipher_text.append(key[i].upper())
+            else:
+                cipher_text.append(key[i])
+        except ValueError:
+            cipher_text.append(item)
     return ''.join(cipher_text)
