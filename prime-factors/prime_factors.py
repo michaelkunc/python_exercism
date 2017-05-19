c6633131cee1b2c2af91f339646f1c948979238a
michaelkunc <mjkunc@gmail.com>
2017-05-18 17:39:59 -0700

    prime factor solution

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,12 +1,11 @@
 def prime_factors(num):
     i = 2
     factors = []
-    while i * i < num:
-        if num % i:
-            i += 1
-        else:
-            num //= i
+    while i * i <= num:
+        while (num % i) == 0:
             factors.append(i)
+            num //= i
+        i += 1
     if num > 1:
         factors.append(num)
     return factors
