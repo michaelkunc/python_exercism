2611d4a272dfd6bfbc9e0c1793da4159d7a62b21
michaelkunc <mjkunc@gmail.com>
2017-05-21 09:00:37 -0700

    re-jigger caesar and cipher

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -3,18 +3,10 @@ import string
 
 class Cipher(object):
 
-    def __init__(self, key):
-        self.key = key
-
-    def encode(self, plaintext):
-        c = Caesar()
-        return c.encode(plaintext)
-
-
-class Caesar(object):
     ALPHABET = string.ascii_lowercase
 
-    def __init__(self):
+    def __init__(self, key=None):
+        self._key = key
         self.letter_map = Caesar.ALPHABET[
             self.key:] + Caesar.ALPHABET[:self.key]
 
@@ -34,8 +26,34 @@ class Caesar(object):
 
     @property
     def key(self):
-        return 3
+        return string.ascii_lowercase.index(self._key)
 
-    @key.setter
-    def key(self, key):
-        return Caesar.ALPHABET.index(key)
+
+class Caesar(Cipher):
+
+    def __init__(self):
+        super(Caesar, self).__init__('d')
+
+    # ALPHABET = string.ascii_lowercase
+
+    # def __init__(self, key=None):
+    #     if not key:
+    #         self.key = 3
+    #     else:
+    #         self.key = key
+    #     self.letter_map = Caesar.ALPHABET[
+    #         self.key:] + Caesar.ALPHABET[:self.key]
+
+    # def encode(self, plaintext):
+    #     ciphertext = ''
+    #     for index, char in enumerate(filter(str.isalpha, plaintext)):
+    #         map_index = Caesar.ALPHABET.index(char.lower())
+    #         ciphertext += self.letter_map[map_index]
+    #     return ciphertext
+
+    # def decode(self, ciphertext):
+    #     plaintext = ''
+    #     for index, char in enumerate(filter(str.isalpha, ciphertext)):
+    #         map_index = self.letter_map.index(char.lower())
+    #         plaintext += Caesar.ALPHABET[map_index]
+    #     return plaintext
