5666677df73654427e427963a47668d174f4e892
michaelkunc <mjkunc@gmail.com>
2017-05-23 15:24:11 -0700

    solution for simple cipher

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -1,11 +1,14 @@
-from string import ascii_lowercase
+from string import ascii_lowercase as lower
 import random
 
 
 class Cipher(object):
 
     def __init__(self, key=None):
-        self._key = key
+        if not key or key.isalpha() and key.islower():
+            self._key = key
+        else:
+            raise ValueError('Key must be all lowercase letters')
 
     def encode(self, plain):
         return self._shift_letters(plain, self._shift_length(plain))
@@ -15,21 +18,21 @@ class Cipher(object):
         return self._shift_letters(cipher, negative_offset)
 
     def _shift_length(self, text):
-        return [ascii_lowercase.index(item) for item in self.key] * (int(len(text) / len(self.key)) + 1)
+        return [lower.index(item) for item in self.key] * (int(len(text) / len(self.key)) + 1)
 
     @staticmethod
     def _shift_letters(text, shift_length):
         return_text = ''
         for index, char in enumerate(filter(str.isalpha, text.lower())):
-            i = (ascii_lowercase.index(char) +
+            i = (lower.index(char) +
                  shift_length[index]) % 26
-            return_text += ascii_lowercase[i]
+            return_text += lower[i]
         return return_text
 
     @property
     def key(self):
         if not self._key:
-            return ''.join([random.choice(ascii_lowercase) for i in range(100)])
+            return ''.join([random.choice(lower) for i in range(100)])
         else:
             return self._key
 
