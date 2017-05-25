76348a593189f9ffcfcb3ef5094224ab77efe0f3
michaelkunc <mjkunc@gmail.com>
2017-05-25 15:29:31 -0700

    crypto square solution

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -1,7 +1,7 @@
 from math import sqrt, ceil
 
 
-def encode(plaintext):
-    normalized = plaintext.lower()
+def encode(plain):
+    normalized = ''.join([c for c in plain.lower() if c.isalnum()])
     cols = ceil(sqrt(len(normalized)))
     return ' '.join([normalized[x::cols] for x in range(cols)])
--- crypto-square/crypto_square_test.py
+++ crypto-square/crypto_square_test.py
@@ -14,16 +14,16 @@ class CryptoSquareTest(unittest.TestCase):
     def test_small_imperfect_square(self):
         self.assertEqual(encode('This is easy!'), 'tis hsy ie sa')
 
-    # def test_punctuation_and_numbers(self):
-    #     msg = "1, 2, 3, Go! Go, for God's sake!"
-    #     ciph = '1gga 2ook 3fde gos ors'
-    #     self.assertEqual(encode(msg), ciph)
+    def test_punctuation_and_numbers(self):
+        msg = "1, 2, 3, Go! Go, for God's sake!"
+        ciph = '1gga 2ook 3fde gos ors'
+        self.assertEqual(encode(msg), ciph)
 
-    # def test_long_string(self):
-    #     msg = ("If man was meant to stay on the ground, god would have given "
-    #            "us roots.")
-    #     ciph = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau"
-    #     self.assertEqual(encode(msg), ciph)
+    def test_long_string(self):
+        msg = ("If man was meant to stay on the ground, god would have given "
+               "us roots.")
+        ciph = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau"
+        self.assertEqual(encode(msg), ciph)
 
 
 if __name__ == '__main__':
