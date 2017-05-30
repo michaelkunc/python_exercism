a39649e548e656a7e269d03a653f09ed65ce6828
michaelkunc <mjkunc@gmail.com>
2017-05-30 15:40:57 -0700

    slight refactor

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -18,9 +18,9 @@ def UNEQUAL():
 def check_lists(first_list, second_list):
     if first_list == second_list:
         return EQUAL
-    elif len(first_list) == 0 or all(i in second_list for i in first_list):
+    elif all(i in second_list for i in first_list):
         return SUBLIST
-    elif len(second_list) == 0 or all(i in first_list for i in second_list):
+    elif all(i in first_list for i in second_list):
         return SUPERLIST
     else:
         return UNEQUAL
