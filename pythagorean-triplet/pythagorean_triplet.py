83050a8053066f919196ce5541b47d6bc7d6c217
michaelkunc <mjkunc@gmail.com>
2017-06-30 10:13:19 -0700

    two more tests

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -17,16 +17,13 @@ def primitive_triplets(number):
         return set(results)
 
 
-def triplets_in_range(number):
+def triplets_in_range(start, end):
     results = []
-    a, b, c = 1, 3, 0
-    while c < number:
-        a_ = (a * b) + a
-        c = math.sqrt(a_**2 + b**2)
-        if c == int(c):
-            results.append((b, a_, int(c)))
-        a += 1
-        b += 1
+    for x in range(start, end + 1):
+        for y in range(start, end + 1):
+            for z in range(start, end + 1):
+                if x * x == y * y + z * z:
+                    results.append(tuple(sorted([x, y, z])))
     return set(results)
 
 
