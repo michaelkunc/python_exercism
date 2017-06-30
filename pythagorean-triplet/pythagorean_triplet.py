7e87d4c13c6f8a160a9febaaa8b976054445cf31
michaelkunc <mjkunc@gmail.com>
2017-06-30 09:58:38 -0700

    one more test passing

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -2,16 +2,19 @@ import math
 
 
 def primitive_triplets(number):
-    results = []
-    a, b, c = 1, 3, 0
-    while c < number:
-        a_ = (a * b) + a
-        c = math.sqrt(a_**2 + b**2)
-        if c == int(c):
-            results.append((b, a_, int(c)))
-        a += 1
-        b += 1
-    return set(results)
+    if number % 2 != 0:
+        raise ValueError
+    else:
+        results = []
+        a, b, c = 1, 3, 0
+        while c < number:
+            a_ = (a * b) + a
+            c = math.sqrt(a_**2 + b**2)
+            if c == int(c):
+                results.append((b, a_, int(c)))
+            a += 1
+            b += 1
+        return set(results)
 
 
 def triplets_in_range(number):
