f5826108aa9e430f2ffdcf8208b98889e8e1c0af
michaelkunc <mjkunc@gmail.com>
2019-03-04 12:55:16 -0800

    changing tack

--- yacht/yacht.py
+++ yacht/yacht.py
@@ -1,29 +1,30 @@
 from collections import Counter
 
-# Score categories
-# Change the values as you see fit
-YACHT = 50
-ONES = 1
-TWOS = 2
-THREES = 3
-FOURS = 4
-FIVES = 5
-SIXES = 6
+
+def score(dice, category):
+    return category(dice)
+
+
+def yacht(dice):
+    if len(set(dice)) == 1:
+        return 50
+    else:
+        return 0
+
+
+def numbers(number):
+    return lambda x: sum([i for i in x if i == number])
+
+
+YACHT = yacht
+ONES = numbers(1)
+TWOS = numbers(2)
+THREES = numbers(3)
+FOURS = numbers(4)
+FIVES = numbers(5)
+SIXES = numbers(6)
 FULL_HOUSE = None
-FOUR_OF_A_KIND = None
+FOUR_OF_A_KIND = "four"
 LITTLE_STRAIGHT = None
 BIG_STRAIGHT = None
 CHOICE = None
-
-
-def score(dice, category):
-    item_counts = Counter(dice)
-    if item_counts.most_common(n=1)[0][1] == 5 and category is YACHT:
-        return YACHT
-    if category == FULL_HOUSE:
-        if set(item_counts.values()) == {2, 3}:
-            return sum(dice)
-        else:
-            return 0
-    else:
-        return item_counts[category] * category
