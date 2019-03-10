e0c2cbeaf4283e8b45cc1c8d99443ed261959d49
michaelkunc <mjkunc@gmail.com>
2019-03-10 08:41:19 -0700

    yacht result

--- yacht/yacht.py
+++ yacht/yacht.py
@@ -2,14 +2,13 @@ from collections import Counter
 
 
 def score(dice, category):
-    return category(dice)
+    result = category(dice)
+    return result if result else 0
 
 
 def yacht(dice):
     if len(set(dice)) == 1:
         return 50
-    else:
-        return 0
 
 
 def numbers(number):
@@ -20,8 +19,6 @@ def full_house(dice):
     item_counts = Counter(dice)
     if set(item_counts.values()) == {2, 3}:
         return sum(dice)
-    else:
-        return 0
 
 
 def four_of_a_kind(dice):
@@ -29,8 +26,20 @@ def four_of_a_kind(dice):
     if any(i >= 4 for i in item_counts.values()):
         most_common = item_counts.most_common(n=1)[0]
         return most_common[0] * 4
-    else:
-        return 0
+
+
+def little_straight(dice):
+    if sorted(dice) == [1, 2, 3, 4, 5]:
+        return 30
+
+
+def big_straight(dice):
+    if sorted(dice) == [2, 3, 4, 5, 6]:
+        return 30
+
+
+def choice(dice):
+    return sum(dice)
 
 
 YACHT = yacht
@@ -42,6 +51,6 @@ FIVES = numbers(5)
 SIXES = numbers(6)
 FULL_HOUSE = full_house
 FOUR_OF_A_KIND = four_of_a_kind
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+LITTLE_STRAIGHT = little_straight
+BIG_STRAIGHT = big_straight
+CHOICE = choice
